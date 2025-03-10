f5c958685c77935185aaa96e70e9456eaad418fb
Michael Schultz <michaelwschultz@gmail.com>
2025-03-10 02:04:24 -0700

    adds frame list fetching and python frame param to load image by name

--- fetch_and_display_image.py
+++ fetch_and_display_image.py
@@ -1,31 +1,31 @@
 import json
 import requests
 import time
+import argparse
 from send_to_display import send_image_to_display
 
 STATE_FILE = "state.json"
-
-# Must list all frames here
-FRAME_STATES = ["weather", "hemolog"]
-
 START_TIME = time.time()
 
-def get_current_state():
+def get_current_state(frame_list):
     # Read the last displayed state from the file.
     try:
         with open(STATE_FILE, "r") as file:
             data = json.load(file)
-            return data.get("last_displayed", 0) # 0 is a default incase last_displayed is missing
+            current_frame = data.get("last_displayed", 0) # 0 is a default incase last_displayed is missing
+            print(f"Current frame is {frame_list[current_frame]}")
+            return current_frame
     except (FileNotFoundError, json.JSONDecodeError):
         return 0  # Default state if file doesn't exist
 
-def set_next_state(current_state):
+def set_next_state(frame_list, current_state):
     # Determine the next state and update the file
-    next_state = current_state + 1 if current_state + 1 < len(FRAME_STATES) else 0
+    next_state = current_state + 1 if current_state + 1 < len(frame_list) else 0
 
     with open(STATE_FILE, "w") as file:
         json.dump({"last_displayed": next_state}, file)
 
+    print(f"Trasitioning to {frame_list[next_state]}, length of states: {len(frame_list)}, state + 1: {current_state + 1}")
     return next_state
 
 def display_image(image_type):
@@ -34,8 +34,26 @@ def display_image(image_type):
     subprocess.run(["python", "send_image_to_display.py"])  # Replace with actual display script
 
 
-def fetch_image(next_state):
-    url = f"https://michaelwschultz-generateframeimage.web.val.run?frame={FRAME_STATES[next_state]}&generate=image"
+def fetch_frame_list():
+    url = "https://michaelwschultz-generateframeimage.web.val.run?generate=list"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            print(f"Possible frame states updated {response.json()}")
+            return response.json()
+        else:
+            print(f"Failed to fetch list of frames. Status code: {response.status_code}")
+            return ["weather", "hemolog"]
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+def fetch_image(frame_list, next_state):
+    url = f"https://michaelwschultz-generateframeimage.web.val.run?frame={frame_list[next_state]}&generate=image"
     output_path = "images/fetched-image.png"
 
     try:
@@ -47,6 +65,7 @@ def fetch_image(next_state):
                     file.write(chunk)
                 elapsed_time = time.time() - START_TIME
                 print(f"Image saved to {output_path}. Took {elapsed_time:.2f} seconds")
+                return
         else:
             print(f"Failed to fetch the image. Status code: {response.status_code}")
 
@@ -54,12 +73,21 @@ def fetch_image(next_state):
         print(f"An error occurred: {e}")
 
 
-def main():
-    current_state = get_current_state()
-    next_state = set_next_state(current_state)
-    fetch_image(next_state)
+def main(frame):
+    if frame is not None:
+        fetch_image([frame], 0)
+    else:
+        frame_list = fetch_frame_list()
+        current_state = get_current_state(frame_list)
+        next_state = set_next_state(frame_list, current_state)
+        fetch_image(frame_list, next_state)
+
     send_image_to_display()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Skip cycle and display any frame by name.")
+    parser.add_argument("-F", "--frame", type=str, required=False, help="Name of the frame to display that matches list.")
+
+    args = parser.parse_args()
+    main(args.frame)
