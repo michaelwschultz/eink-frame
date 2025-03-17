83d2f4e1b17a55107b7b5efaf0140d429a579744
Michael Schultz <michaelwschultz@gmail.com>
2025-03-17 01:09:57 -0700

    adds red threshold variable and fixes clear bug

--- send_to_display.py
+++ send_to_display.py
@@ -4,12 +4,14 @@
 
 import sys
 import time
-from PIL import Image
 
+from PIL import Image
 from waveshare_epd import epd7in5b_V2
+
 
 EPD_WIDTH = 800  # pixels
 EPD_HEIGHT = 480  # pixels
+RED_THRESHOLD = 60
 
 # Define the picture directory
 LOCAL_IMG = "/images/fetched-image.png"
@@ -31,7 +33,7 @@ def load_image():
         for j in range(image.size[1]):
             r, g, b = pixels[i, j]
             # Check if red is the dominant color
-            if (r - g >= 80) and (r - b >= 80):
+            if (r - g >= RED_THRESHOLD) and (r - b >= RED_THRESHOLD):
                 # Keep the red pixel as is, make it fully opaque in the new image
                 new_pixels[i, j] = (0, g, b)
             else:
@@ -46,16 +48,16 @@ def load_image():
     return image, red_channel
 
 
-async def send_image_to_display():
+def send_image_to_display():
     """Send the image to the E-Ink display."""
     start_time = time.time()
     print("Initializing EPD...")
     epd = epd7in5b_V2.EPD()
     epd.init()
-    epd.Clear(0xFF)  # Clear the display with white color
+    # epd.Clear() # clear the display if needed
 
     # Load the image and separate black and red channels
-    black_channel, red_channel = await load_image()
+    black_channel, red_channel = load_image()
 
     frame_black = epd.getbuffer(black_channel)
     frame_red = epd.getbuffer(red_channel)
@@ -69,12 +71,13 @@ async def send_image_to_display():
         print(f"Image sent to display. Took {elapsed_time:.2f} seconds")
     except IOError as e:
         print(f"Sending image to display failed: {e}")
+        epd.sleep()
+        epd.Dev_exit()
     except KeyboardInterrupt:
         print("Ctrl + C: Exiting...")
-        epd.Dev_exit()
         epd.sleep()
-
-    sys.exit()
+        epd.Dev_exit()
+        sys.exit()
 
 
 if __name__ == "__main__":
